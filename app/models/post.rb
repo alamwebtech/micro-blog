0ecd28db935774e1e142de067c56d798a8f4fc0b
msalam <ms17alam@gmail.com>
2017-12-02 12:28:03 -0500

    post page update

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,7 @@ has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 3}
   validates :body, presence: true
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  
 
 
 end
